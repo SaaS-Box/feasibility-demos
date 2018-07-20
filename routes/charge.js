19dd5072e856b904d2770bc31bdbf0b6b819b345
zhaolei@easyact.cn <zhaolei@easyact.cn>
2018-07-20 16:33:20 +0800

    -button of charge

--- routes/charge.js
+++ routes/charge.js
@@ -1,19 +1,13 @@
-var express = require('express')
-var router = express.Router()
-var stripe = require('stripe')('sk_test_z52YuKBf6gGMLsv3lagY1Jkx')
+const express = require('express')
+const router = express.Router()
+const stripe = require('stripe')('sk_test_z52YuKBf6gGMLsv3lagY1Jkx')
 
 router.get('/', function (req, res, next) {
-    console.log(req.query)
-    res.cookie('stripe', req.query)
-    res.render('charge', {title: 'Confirm your payment'})
-})
-
-router.post('/', async function (req, res, next) {
-    console.log('Charging')
+    console.log('Charging', req.query)
     stripe.charges.create({
         amount: 100,
         currency: 'usd',
-        source: req.cookies.stripe.source,
+        source: req.query.source,
     })
         .then(charge => res.render('success', {title: 'pay success!', desc: JSON.stringify(charge)}))
         .catch(err => {
